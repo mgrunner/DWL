e03b32088183127a02c3a3c935b92cb599002ea6
mgrunner <95408903+mgrunner@users.noreply.github.com>
2024-01-02 13:39:11 +0100

    Add comments to lambda_free_float_location.py

--- lambda_data_lake/lambda_free_float_location.py
+++ lambda_data_lake/lambda_free_float_location.py
@@ -1,3 +1,4 @@
+# Import necessary packages
 import json
 import pandas as pd
 import numpy as np
@@ -7,33 +8,35 @@ import datetime
 import psycopg2
 import os
 
+# Access details for database
 ENDPOINT = os.environ['ENDPOINT']
 DB_NAME = os.environ['DB_NAME']
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
 
+# Data sources for shared mobility services and polygons of neigborhoods in Zurich
 url_free_float = 'https://sharedmobility.ch/free_bike_status.json'
 url_provider = 'https://sharedmobility.ch/providers.json'
 fp = "https://www.ogd.stadt-zuerich.ch/wfs/geoportal/Statistische_Quartiere?service=WFS&version=1.1.0&request=GetFeature&outputFormat=GeoJSON&typename=adm_statistische_quartiere_v"
 
-
+# Lambda function
 def lambda_handler(event, context):
-
+    # Connect to database using access details defined above
     try:
         print("host={} dbname={} user={} password={}".format(ENDPOINT, DB_NAME, USERNAME, PASSWORD))
         conn = psycopg2.connect("host={} dbname={} user={} password={}".format(ENDPOINT, DB_NAME, USERNAME, PASSWORD))
-
     except psycopg2.Error as e:
         print("Error: Could not make connection to the Postgres database")
         print(e)
-
+    
+    # Create database cursor
     try:
         cur = conn.cursor()
     except psycopg2.Error as e:
         print("Error: Could not get curser to the Database")
         print(e)
 
-    # Auto commit 
+    # Auto commit changes to database
     conn.set_session(autocommit=True)
     
     # Get data on vehicles
@@ -42,11 +45,12 @@ def lambda_handler(event, context):
     last_updated = r.json()['last_updated']
     # Convert to datetime and adjust to local time
     dt = datetime.datetime.fromtimestamp(last_updated) + datetime.timedelta(hours=1)
+    # Get data on vehicles
     data = pd.DataFrame(r.json()['data']['bikes'])
-    # Add the timestamp as additional column
+    # Add the timestamp as additional column to vehicle data
     data['last_updated'] = dt
 
-    # Get data on provider
+    # Get data on providers
     r = requests.get(url_provider)
     data_provider = pd.DataFrame(r.json()['data']['providers'])
 
@@ -55,24 +59,24 @@ def lambda_handler(event, context):
     # Filter only area around Zurich
     data = data[((data['lat']>47.31460345103779) & (data['lat']<47.43742324709611)) & ((data['lon']>8.440539921109622)&(data['lon']<8.634235720318037))]
     # Filter only relevant providers
-    providers = ['bird-zurich-escooter', 'tier', 'voiscooters.com'] #'lime_zurich_ebike', 'lime_zurich_escooter', 'lime_opfikon_ebike', 'lime_opfikon_escooter'
+    providers = ['bird-zurich-escooter', 'tier', 'voiscooters.com']
     data = data[data['provider_id'].isin(providers)]
     # Change to geodata-format
     data = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.lon,data.lat))
 
     # Load geodata from Zurich
     map_df = gpd.read_file(fp)
-    # Change projection system
-    esg_epsg = 4326 # # EPSG code for ESG
+    # Change projection system to match the one of vehicle data
+    esg_epsg = 4326 # EPSG code for ESG
     map_df = map_df.to_crs(epsg=esg_epsg)
 
-    # Assign locations to Quartiere
+    # Assign locations to neighborhoods
     data['qname'] = np.nan
     data['qnr'] = ''
     for idx in range(map_df.shape[0]):
-        #For every address, find if they reside within a province
+        # For every address, find if they reside within a neigburhood
         pip = data.within(map_df.loc[idx, 'geometry'])
-        if pip.sum() > 0: #we found where some of the addresses reside at map_df.loc[idx]
+        if pip.sum() > 0: # we found where some of the addresses reside at map_df.loc[idx]
             data.loc[pip, 'qname']  = map_df.loc[idx, 'qname']
             data.loc[pip, 'qnr']  = map_df.loc[idx, 'qnr']
 
@@ -80,9 +84,11 @@ def lambda_handler(event, context):
     data.dropna(subset=['qname'], inplace=True)
     print(data.head(5))
 
+    # Create table in database if it doesn't already exist
     cur.execute("CREATE TABLE IF NOT EXISTS public.free_floating_systems_loc (vehicle_id varchar, provider_id varchar, lat float8, lon float8, \
         is_disabled boolean, is_reserved boolean, last_updated timestamp, qname varchar, qnr int);")
-
+    
+    # Load the data to the database
     try:
         for row in data.iterrows():
             #print(row[1])
@@ -92,6 +98,7 @@ def lambda_handler(event, context):
         print("Error: Inserting Rows")
         print (e)
 
+    # Close database connection
     cur.close()
     conn.close()
     
